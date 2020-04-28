0ffe962745b5c5f2152d0a24d25d1ea5d2a3d09e
mohamadtarekk <mohamadtareksh@gmail.com>
2020-04-28 09:53:57 +0200

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         if (args.length == 0)
-            args = new String[]{"ast", "1,2,0,3,4,5,6,7,8", "man"};
+            args = new String[]{"ast", "1,2,3,7,0,4,5,6,8,9,10,11,12,13,14,15"};
         GameDriver gameDriver = new GameDriver();
         gameDriver.run(args);
     }
